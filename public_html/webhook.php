962e1b6710efbdf78acd3d1f9b4fb709b2bac69c
Patrice de Saint Steban <patrice.desaintsteban@zenika.com>
2019-01-24 17:02:47 +0100

    webhook

---
+++ public_html/webhook.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once("../includes/braintree_init.php");
+
+if (
+  isset($_POST["bt_signature"]) &&
+  isset($_POST["bt_payload"])
+) {
+  $webhookNotification = $gateway->webhookNotification()->parse(
+      $_POST["bt_signature"], $_POST["bt_payload"]
+  );
+
+  // Example values for webhook notification properties
+  $message = $webhookNotification->kind; // "subscription_went_past_due"
+  $message .= ": ";
+  $message .= $webhookNotification->timestamp->format('D M j G:i:s T Y'); // "Sun Jan 1 00:00:00 UTC 2012"
+
+  error_log($message);
+
+  header("HTTP/1.1 200 OK");
+}
+?>
